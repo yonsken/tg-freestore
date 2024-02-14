bdc1ecea6f0e0fd51227de819cc3c54d83badcbb
Maksimiljans Januska <yanumdev@gmail.com>
2024-02-14 23:16:26 +0000

    Implement basic telegram bot

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yonsken/tg-freestore
 
 go 1.22.0
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	botapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	bot, err := botapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
+	if err != nil {
+		panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := botapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := botapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
