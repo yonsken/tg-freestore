92bb8df2bbaa35f01c540491156347645d3441fc
Maksimiljans Januska <yanumdev@gmail.com>
2024-02-15 00:30:42 +0000

    Implement bot that responds with local image

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"path/filepath"
 
 	botapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -23,15 +24,46 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message == nil {
-			continue
+		if err := processUpdate(bot, update); err != nil {
+			panic(err)
 		}
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := botapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		bot.Send(msg)
 	}
+}
+
+func processUpdate(bot *botapi.BotAPI, update botapi.Update) error {
+	if update.Message == nil {
+		return nil
+	}
+
+	var (
+		userName = update.Message.From.UserName
+		chatID   = update.FromChat().ChatConfig().ChatID
+	)
+
+	log.Printf("Update message from [%s]: %s", userName, update.Message.Text)
+
+	fileReader, err := os.Open(filepath.Clean("test/gopher.png"))
+	if err != nil {
+		return err
+	}
+
+	file := botapi.FileReader{
+		Name:   "gopher.png",
+		Reader: fileReader,
+	}
+
+	mediaGroup := botapi.NewMediaGroup(chatID, []any{botapi.NewInputMediaPhoto(file)})
+
+	messages, err := bot.SendMediaGroup(mediaGroup)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Successfully sent test file to chat %d with [%s]", chatID, userName)
+
+	for i, msg := range messages {
+		log.Printf("Send Media Group message %d: %s", i, msg.Text)
+	}
+
+	return nil
 }
